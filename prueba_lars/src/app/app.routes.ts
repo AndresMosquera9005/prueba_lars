bb709d918ba36849c5ad71478a5493db4012e286
AndresMosquera9005 <andrubjj@gmail.com>
2024-07-13 11:19:59 -0500

    se creo componentes header, body y se modificaron

--- prueba_lars/src/app/app.routes.ts
+++ prueba_lars/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: BodyComponent
+    }
+];
